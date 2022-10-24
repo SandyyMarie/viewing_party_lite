16fc8fca3fece07a33ab4d080eeac4b5e1285c9c
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-10-24 14:31:19 -0600

    refactor: updated factory bot with pasword filling along with updating fight_club fixture to represent current stats

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   has_many :user_parties
   has_many :parties, through: :user_parties
+
+  has_secure_password
 end
--- spec/factories/factory_bot.rb
+++ spec/factories/factory_bot.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :user, class: User do
     name { Faker::Name.name }
     email { Faker::Internet.email }
+    password { Faker::Internet.password }
   end
 
   factory :party, class: Party do
