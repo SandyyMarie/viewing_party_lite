9ccab7d1cc48ad7e804c7aae7d648acb05254ec2
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-10-24 14:10:58 -0600

    test: added validation requirements for email and password along with testing for password digest

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,9 +6,19 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
+    it { should validate_uniqueness_of(:email)}
+    it { should validate_presence_of(:password_digest)} #-- Remember, bcrypt will check out passwords, and store it as `password_digest`
+    it { should have_secure_password}
   end
+
   describe 'relationships' do
     it { should have_many :user_parties }
     it { should have_many(:parties).through(:user_parties) }
+  end
+  
+  it 'ensures password digest works' do
+    user = User.create(name: 'Meg', email: 'meg@test.com', password: 'password123', password_confirmation: 'password123')
+    expect(user).to_not have_attribute(:password)
+    expect(user.password_digest).to_not eq('password123')
   end
 end
