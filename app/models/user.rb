54aa4fd2c295508527228a193a453ea73d36554d
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-10-24 14:20:57 -0600

    refactor: updated model to now have password_digest and updated validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates_presence_of :name, :email
+  validates_presence_of :name
+  validates_presence_of :password
+  validates :email, uniqueness: true, presence: true
   has_many :user_parties
   has_many :parties, through: :user_parties
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of(:email)}
-    it { should validate_presence_of(:password_digest)} #-- Remember, bcrypt will check out passwords, and store it as `password_digest`
+    it { should validate_presence_of(:password)} # Can also be :password_digest-- Remember, bcrypt will check out passwords, and store it as `password_digest`
     it { should have_secure_password}
   end
 
